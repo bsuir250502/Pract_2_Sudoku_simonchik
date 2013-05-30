5323b614917c2c67c690fc5ac61b515911ac6ffc
ddzyanis <ddzyanis@mail.ru>
2013-05-31 03:45:00 +0400

    new sudoku

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 
 #define SIZE 9
+
 int field[SIZE][SIZE];
 
 void InputField()
 {
+    int i, j;
     FILE *infile;
     infile = fopen("sudoku_in", "r");
-    int i, j;
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++) {
             field[i][j] = getc(infile) - '0';
@@ -18,17 +19,15 @@ void InputField()
     fclose(infile);
 }
 
-
 void DisplaySudoku()
-    {
-    int i, j;     
-    printf("====================\n\n");    
+{
+    int i, j;
+    printf("====================\n\n");
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++) {
             if (j == 2 || j == 5 || j == 8) {
                 printf("%2d ", field[i][j]);
-            }
-            else {
+            } else {
                 printf("%2d", field[i][j]);
             }
         }
@@ -36,14 +35,14 @@ void DisplaySudoku()
         if (i == 2 || i == 5 || i == 8) {
             puts("");
         }
-    }        
+    }
 }
 
 void FOutputField()
 {
+    int i, j;
     FILE *outfile;
     outfile = fopen("sudoku_out", "w");
-    int i, j;
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++) {
             fprintf(outfile, "%d", field[i][j]);
@@ -53,50 +52,26 @@ void FOutputField()
     fclose(outfile);
 }
 
-int CheckStrings()
+int CheckCellXCellY()
 {
-    int i, j, k;
-    int values[SIZE];
-    for (i = 0; i < SIZE; ++i) {        
-        for (k = 0; k < SIZE; k++) {
-            values[k] = 0;       
-        }
-        for (j = 0; j < SIZE; ++j) {
-            if (!field[i][j]) {
-                continue;
-            }
-            if (!values[field[i][j] - 1]) {
-                values[field[i][j] - 1] = 1;
-            }
-            else {
-                return 1;
+    int x, y, i, j;
+    for (y = 0; y < SIZE; y++) {
+        for (x = 0; x < SIZE; x++) {
+            if (field[y][x]) {
+                for (j = 0; j < SIZE; j++) {
+                    if (field[y][x] == field[y][j] && j != x) {
+                        return 0;
+                    }
+                }
+                for (i = 0; i < SIZE; i++) {
+                    if (field[y][x] == field[i][x] && i != y) {
+                        return 0;
+                    }
+                }
             }
         }
     }
-    return 0;
-}
-
-int CheckColumns()
-{
-    int j, i, k;
-    int values[SIZE];
-    for (j = 0; j < SIZE; ++j) {        
-        for (k = 0; k < SIZE; k++) {
-            values[k] = 0;
-        }
-        for (i = 0; i < SIZE; ++i) {
-            if (!field[i][j]) {
-                continue;
-            }
-            if (!values[field[i][j] - 1]) {
-                values[field[i][j] - 1] = 1;
-            }
-            else {
-                return 1;
-            }
-        }
-    }
-    return 0;
+    return 1;
 }
 
 int CheckBoxes()
@@ -104,23 +79,32 @@ int CheckBoxes()
     int i, j, k, ik, jk;
     int values[SIZE];
     for (i = 0; i < SIZE; i += 3) {
-        for (j = 0; j < SIZE; j += 3) {            
+        for (j = 0; j < SIZE; j += 3) {
             for (k = 0; k < SIZE; k++) {
                 values[k] = 0;
-            }            
-            for (ik = i; ik < i + 3; ++ik) {                
+            }
+            for (ik = i; ik < i + 3; ++ik) {
                 for (jk = j; jk < j + 3; ++jk) {
                     if (!field[ik][jk]) {
                         continue;
                     }
                     if (!values[field[ik][jk] - 1]) {
                         values[field[ik][jk] - 1] = 1;
-                    }
-                    else {
-                        return 1;
+                    } else {
+                        return 0;
                     }
                 }
             }
+        }
+    }
+    return 1;
+}
+
+int Check()
+{
+    if (CheckCellXCellY()) {
+        if (CheckBoxes()) {
+            return 1;
         }
     }
     return 0;
@@ -128,47 +112,44 @@ int CheckBoxes()
 
 int SolveSudoku(int i, int j)
 {
-    int n = 1;
-    if (j == 9) {
-        j = 0;
-        i++;
-    }
-    if (i == 9) {
-        return 0;
-    }
+    int flag;
+    int correct = 1;
+    int current_value = 1;
     while (1) {
-        if (field[i][j]) {            
-            if ((j + 1) >= SIZE) {
-                i++;
-                j = 0;
-            } 
-            else {
-                j++;
-            }
+        if (j > 8) {
+            i++;
+            j = 0;
         }
-        else {
-            break;
-        }    
-    }
-    while (1) {
-        while (1) {
-            field[i][j] = n;
-            n++;
-            if (!CheckStrings()) {
-                if (!CheckColumns()) {
-                    if (!CheckBoxes()) {
-                        break;
-                    }
-                }
-            }
-            if (n >= 10) {
-                field[i][j] = 0;
-                return 1;
-            }
+        if (i == SIZE) {
+            return 1;
         }
-        if (!SolveSudoku(i, (j + 1))) {
+
+        if (current_value == 1 && field[i][j]) {
+            flag = 1;
+        } else {
+            flag = 0;
+        }
+
+        if (current_value > SIZE) {
+            field[i][j] = 0;
             return 0;
         }
+
+        if (!flag) {
+            field[i][j] = current_value;
+        }
+
+        if (Check()) {
+            correct = SolveSudoku(i, j + 1);
+        }
+        if (!correct || !Check()) {
+            current_value++;
+            continue;
+        }
+        if (flag && !correct) {
+            return 0;
+        }
+        return 1;
     }
 }
 
@@ -176,7 +157,8 @@ int main()
 {
     InputField();
     DisplaySudoku();
-    if (!SolveSudoku(0, 0)) {
+    if (Check()) {
+        SolveSudoku(0, 0);
         DisplaySudoku();
         FOutputField();
     } else {
